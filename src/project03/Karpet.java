a3dae6ccd92a5b57bbcf168b18027bf470a724ce
MasyudiPutra <32509043+MasyudiPutra@users.noreply.github.com>
2018-04-07 13:28:44 +0800

    Buat method cetakinfo()

--- src/project03/Karpet.java
+++ src/project03/Karpet.java
@@ -4,5 +4,11 @@ public class Karpet {
     String merk;
     String warna;
     double harga;
+    
+    void cetakinfo(){
+        System.out.println("Merk Karpet \t: "+merk+"\n"+
+                 "Warna Karpet \t "+warna+"\n"+
+                 "Harga Karpet \t "+harga);
+    }
      
 }
