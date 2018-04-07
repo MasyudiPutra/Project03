469c4e3c72c27989bfe85f58068a2c31a3cca7da
MasyudiPutra <32509043+MasyudiPutra@users.noreply.github.com>
2018-04-07 14:24:44 +0800

    Atribut dan method

--- src/project03/Tikar.java
+++ src/project03/Tikar.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project03;
 
-/**
- *
- * @author ASUS
- */
-public class Tikar {
+public class Tikar extends Karpet {
+    String jenisTikar;
     
+    void throttle(){
+        System.out.println("Masfloor");
+    
+    }
 }
