3a74e5da479676f508d11cde23aca2dbc9045e3a
MasyudiPutra <32509043+MasyudiPutra@users.noreply.github.com>
2018-04-07 14:31:41 +0800

    Getter and Setter

--- src/project03/Tikar.java
+++ src/project03/Tikar.java
@@ -7,4 +7,14 @@ public class Tikar extends Karpet {
         System.out.println("Masfloor");
     
     }
+
+    public String getJenisTikar() {
+        return jenisTikar;
+    }
+
+    public void setJenisTikar(String jenisTikar) {
+        this.jenisTikar = jenisTikar;
+    }
+    
+    
 }
